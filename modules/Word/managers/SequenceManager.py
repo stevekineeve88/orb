f859a7ba25564d660c1988b2d2c876b9c64a0386
Stephen Ayre <stevemamajama@gmail.com>
2021-02-01 13:31:26 -0500

    Better checking for complexity

--- modules/Word/managers/SequenceManager.py
+++ modules/Word/managers/SequenceManager.py
@@ -1,11 +1,17 @@
+import math
+
 from modules.Word.objects.Permutation import Permutation
 
 
 class SequenceManager:
 
+    __COMPLEXITY = 500000
+
     @classmethod
     def search_permutations(cls, characters: str, **kwargs) -> Permutation:
         sequence_length = kwargs.get("sequence_length") or len(characters)
+        if cls.__is_complex(len(characters), sequence_length):
+            raise Exception("Too complex for calculating permutations")
         permutation = Permutation(characters)
         permutation.set_prefixes(kwargs.get("prefixes") or [])
         permutation.set_suffixes(kwargs.get("suffixes") or [])
@@ -17,4 +23,4 @@ class SequenceManager:
 
     @classmethod
     def __is_complex(cls, n: int, r: int) -> bool:
-        pass
+        return math.factorial(n) / math.factorial(n-r) > cls.__COMPLEXITY
--- modules/Word/objects/Permutation.py
+++ modules/Word/objects/Permutation.py
@@ -48,42 +48,42 @@ class Permutation:
     def set_excludes(self, excludes: list):
         self.__excludes = excludes
 
-    def __has_prefixes(self, characters: str) -> bool:
+    def __check_prefixes(self, characters: str) -> bool:
         prefixes = self.__prefixes
         for prefix in prefixes:
             if characters.startswith(prefix):
                 return True
         return len(prefixes) == 0
 
-    def __has_suffixes(self, characters: str) -> bool:
+    def __check_suffixes(self, characters: str) -> bool:
         suffixes = self.__suffixes
         for suffix in suffixes:
             if characters.endswith(suffix):
                 return True
         return len(suffixes) == 0
 
-    def __has_contains(self, characters: str) -> bool:
+    def __check_contains(self, characters: str) -> bool:
         contains = self.__contains
         for contain in contains:
             if contain in characters:
                 return True
         return len(contains) == 0
 
-    def __has_regexes(self, characters: str) -> bool:
+    def __check_regexes(self, characters: str) -> bool:
         regexes = self.__regexes
         for regex in regexes:
-            if self.__is_matching(characters, regex):
+            if self.__is_matching_regex(characters, regex):
                 return True
         return len(regexes) == 0
 
-    def __not_has_excludes(self, characters: str) -> bool:
+    def __check_excludes(self, characters: str) -> bool:
         excludes = self.__excludes
         for regex in excludes:
-            if self.__is_matching(characters, regex):
+            if self.__is_matching_regex(characters, regex):
                 return False
         return True
 
-    def __is_matching(self, characters: str, regex: str) -> bool:
+    def __is_matching_regex(self, characters: str, regex: str) -> bool:
         if len(regex) != len(characters):
             return False
         for i in range(0, len(regex)):
@@ -91,14 +91,14 @@ class Permutation:
                 return False
         return True
 
-    def __is_allowed(self, characters: str) -> bool:
-        return self.__has_prefixes(characters) and \
-            self.__has_suffixes(characters) and \
-            self.__has_contains(characters) and \
-            self.__has_regexes(characters) and \
-            self.__not_has_excludes(characters)
+    def __checks_pass(self, characters: str) -> bool:
+        return self.__check_prefixes(characters) and \
+               self.__check_suffixes(characters) and \
+               self.__check_contains(characters) and \
+               self.__check_regexes(characters) and \
+               self.__check_excludes(characters)
 
-    def __check_correct(self, prefix: str) -> bool:
+    def __check_correct_permutation(self, prefix: str) -> bool:
         prefix_dict = {}
         for character in prefix:
             prefix_dict[character] = (prefix_dict[character] + 1) if character in prefix_dict else 1
@@ -108,11 +108,11 @@ class Permutation:
 
     def __get_permutations(self, characters, prefix, n, k):
         if k == 0:
-            if self.__is_allowed(prefix):
+            if self.__checks_pass(prefix):
                 self.__result[prefix] = (self.__result[prefix] + 1) if prefix in self.__result else 1
             return
 
         for i in range(n):
             new_prefix = prefix + characters[i]
-            if self.__check_correct(new_prefix):
+            if self.__check_correct_permutation(new_prefix):
                 self.__get_permutations(characters, new_prefix, n, k - 1)
